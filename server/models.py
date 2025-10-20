6ae9af62ecbe3164e75a68cb5b9e2629a73e762f
Sharon <sharonirungu2006@gmail.com>
2025-10-20 09:39:49 +0300

    Add property model

--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-class User(db.Model, SerializerMixin):
+class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -6,5 +6,18 @@ class User(db.Model, SerializerMixin):
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
     role = db.Column(db.String, doc="admin", "landlord", "tenant")
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Property(db.Model):
+    __tablename__ = "properties"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullabe=False)
+    description = db.Column(db.String, nullable=False)
+    rent_price = db.Column(db.String, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String)
+    landlord_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    available = db.Column(db.Boolean, default=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
